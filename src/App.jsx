fd549e3b5e6bd35e3dd2a059af32da2ca7a67243
Guillermo Jimenez Iriarte <122047330+GuilleJI@users.noreply.github.com>
2024-02-26 23:57:06 -0400

    Setup react, vite and tailwind + setting up color

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return <>
+  <h1 className="text-3xl font-bold text-primary">
+      Hello world!
+    </h1>
+    {/* our color setup is done */}
+    {/* npw setup fonts */}
+  </>;
+}
+
+export default App
