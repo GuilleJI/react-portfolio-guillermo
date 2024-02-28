81f337286a3bc29f916f23bdab48934455878275
Guillermo Jimenez Iriarte <122047330+GuilleJI@users.noreply.github.com>
2024-02-28 01:59:29 -0400

    Finished Setting up Home page

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Home from './components/Home';
 import Navbar from './components/Navbar';
 
 function App() {
   return <>
       <Navbar/>
+      <Home />
   </>;
 }
 //10:17
---
+++ src/components/Home.jsx
@@ -0,0 +1,22 @@
+import bannerImg from '../images/Car.jpg'
+
+const Home = () => {
+    return (
+      <div className="mt-20 bg-bgShade">
+        <div className="lg:px-12 px-4 flex flex-col md:flex-row-reverse items-center justify-between py-24 gap-5">
+          <div className="md:w-1/2 w-full">
+                <img src={bannerImg} alt="" className="'w-full'" />
+          </div>
+
+           <div className="md:w-1/2 w-full mt-5">
+              <h3 className="text-xl text-headingColor font-semibold mb-5">Hey, I'm Guillermo</h3>
+              <h1 className="md:text-6xl text-4xl font-bold text-headingColor leading-snug md:leading-[76px] mb-5">I'm an <span className="text-primary">IT Programming Student </span> at NSCC</h1>
+              <p className="text-body text-2xl leading-9 mb-8">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit, nihil!</p>
+              <button className="btn-primary">Get In Touch</button>
+           </div>            
+        </div>
+      </div>
+    );
+};
+
+export default Home; 
