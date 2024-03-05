d885caec46553c689ca4cf77f0e596cc182393b4
Guillermo Jimenez Iriarte <122047330+GuilleJI@users.noreply.github.com>
2024-03-04 23:57:16 -0400

    added space between the skills descriptions and the bottom part of skills cards

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -27,7 +27,7 @@ const Skills = () => {
                     skills.map(skill => <div key={skill.id} className="bg-bgShade p-8 rounded-lg cursor-pointer hover:-translate-y-4 transition-all duration-300 skillcard">
                         <img src={skill.image} alt="" className="w-30 h-30 bg-white rounded-lg shadow-md mb-7 p-3"/>
                         <h3 className="text-2x1 font-bold mb-4">{skill.name}</h3>
-                        <p>{skill.description}</p>
+                        <p>{skill.description}</p><br/>
                     </div>)
                 }
             </div> 
