a93751693bb853d6a3e9814b06ce84d5fb4e2685
Guillermo Jimenez Iriarte <122047330+GuilleJI@users.noreply.github.com>
2024-03-07 23:33:47 -0400

    Added description to my About me page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,14 +12,14 @@ const About = () => {
                     <p className='text-xl font-semibold mb-5'>About</p>
                     <h2 className='md:text-5xl text-4xl font-bold'>About Me</h2>
                     <p className='mt-8 md:pr-8 mb-8'>
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere hic iste voluptatibus illo deleniti.
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere hic iste voluptatibus illo deleniti.
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere hic iste voluptatibus illo deleniti.
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere hic iste voluptatibus illo deleniti. <br /><br />
-
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere hic iste voluptatibus illo deleniti.
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere hic iste voluptatibus illo deleniti.
-                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere hic iste voluptatibus illo deleniti.
+                    Hi, I’m Guillermo Jimenez Iriarte, studying IT Programming at Nova Scotia Community College (NSCC) in Canada.
+                    I love the creativity and constant changes in IT, which led me to this field. 
+                    At NSCC, I’ve worked on important projects and learned a lot about programming languages and IT concepts.
+                    I’m good at JavaScript, C#, Kotlin, Java, and a bit of C++, and I understand data structures, algorithms, and web development.
+                    Outside school, I like to learn about new technologies and IT trends, and I enjoy improving my programming skills.
+                    <br/><br/>
+                    In addition to coding, I have a keen interest in reading about, learning, and analyzing historical events. I also enjoy sketching to improve and scale my creativity, staying active through gym workouts and outdoor activities, and camping to connect with nature and people. 
+                    Feel free to explore my portfolio and don’t hesitate to reach out if you have any questions or would like to connect.”
                     </p>
                     <Link
                         to="contact"
