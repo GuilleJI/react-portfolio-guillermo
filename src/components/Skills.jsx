be57c2c08f04acdde4a55624eaf4e0f4ec1cbc87
Guillermo Jimenez Iriarte <122047330+GuilleJI@users.noreply.github.com>
2024-03-14 02:44:23 -0300

    modified skills page

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -7,13 +7,13 @@ const skills =[
     {id: 3, name: "Data Management", description: "Familiar with data fundamentals, including SQL, ADO.NET, and Entity Framework.", image: "src/images/Data Management.png"},
     {id: 4, name: "Mobile Development", description: "Experienced in Java and Kotlin for Android development using Android Studio.", image: "src/images/Mobile Development.png"},
     {id: 5, name: "Version Control", description: "Proficient in Git, with experience in managing GitHub projects including branches and commits.", image: "src/images/Version Control.png"},
-    {id: 6, name: "Design Tools", description: "Proficient in Figma for interface design, prototyping, and real-time collaboration.", image: "src/images/FigmaDesign.png"},
+    {id: 6, name: "Design Tools", description: "Proficient in Figma for interface design, prototyping, and real-time collaboration. Furthermore, I am adept at using Figma's vector editing capabilities and components feature to create reusable design elements, ensuring consistency across all designs.", image: "src/images/FigmaDesign.png"},
     {id: 7, name: "Professional Practices for IT", description: "Understanding of professional standards, ethics, and practices in the IT industry.", image: "src/images/IT.png"},
     {id: 8, name: "Project Management", description: "Experience collaborating projects from conception to completion, often a key component of coursework. ", image: "src/images/Management.png"},
     {id: 9, name: "Problem-solving", description: "Experienced in applying logical reasoning to tackle intricate challenges, a proficiency developed through extensive involvement in programming and systems analysis tasks.", image: "src/images/Problem Solving.png"},
     {id: 10, name: "Teamwork", description: "Accustomed to collaborative environments, with a focus on projects that require team synergy and effective communication.", image: "src/images/Teamwork.png"},
     {id: 11, name: "Attention to Detail", description: "Exhibit strong proficiency in time management and exceptional organizational abilities.", image: "src/images/Detail.png"},
-    {id: 12, name: "Eagerness to Learn", description: "Proactively pursuing new experiences and knowledge to enhance exiting skills.", image: "src/images/Learning.png"},
+    {id: 12, name: "Eagerness to Learn", description: "Proactively pursuing new experiences and knowledge to enhance existing skills.", image: "src/images/Learning.png"},
 ]
 const Skills = () => {
     return (
