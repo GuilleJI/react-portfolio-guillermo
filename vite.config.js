9ed74fe84ce638053e4dad4ffe9bc0fa0b658860
Guillermo Jimenez Iriarte <122047330+GuilleJI@users.noreply.github.com>
2024-03-15 16:49:13 -0300

    changes made

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/react-portfolio-guillermo/',
+  base: '/react-portfolio-guillermo/', // Adjust this to match your deployment path
   plugins: [react()],
-})
+});
