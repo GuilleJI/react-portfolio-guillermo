a93b4eede599f15c9f841b4c09c8a1b2026fcae1
Guillermo Jimenez Iriarte <122047330+GuilleJI@users.noreply.github.com>
2024-03-15 17:25:13 -0300

    sadasd

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,18 +2,18 @@ import {Link} from 'react-scroll';
 
 // Making some array of objects for card items
 const skills =[
-    {id: 1, name: "Programming Languages", description: "Experienced in a diverse range of programming languages, including but not limited to JavaScript, Kotlin, Java, C#, C, and C++. This broad knowledge base allows for flexibility and adaptability when tackling various software development tasks.", image: "src/images/Programming.png"},
-    {id: 2, name: "Web Development", description: "Proficient in website design and development using HTML, CSS (SASS, Tailwind CSS, Bootstrap),JavaScript (React.js, Node.js), and C# (ASP.NET).", image: "src/images/Web Dev.png"},
-    {id: 3, name: "Data Management", description: "Familiar with data fundamentals, including SQL, ADO.NET, and Entity Framework.", image: "src/images/Data Management.png"},
-    {id: 4, name: "Mobile Development", description: "Experienced in Java and Kotlin for Android development using Android Studio.", image: "src/images/Mobile Development.png"},
-    {id: 5, name: "Version Control", description: "Proficient in Git, with experience in managing GitHub projects including branches and commits.", image: "src/images/Version Control.png"},
-    {id: 6, name: "Design Tools", description: "Proficient in Figma for interface design, prototyping, and real-time collaboration. Furthermore, I am adept at using Figma's vector editing capabilities and components feature to create reusable design elements, ensuring consistency across all designs.", image: "src/images/FigmaDesign.png"},
-    {id: 7, name: "Professional Practices for IT", description: "Understanding of professional standards, ethics, and practices in the IT industry.", image: "src/images/IT.png"},
-    {id: 8, name: "Project Management", description: "Experience collaborating projects from conception to completion, often a key component of coursework. ", image: "src/images/Management.png"},
-    {id: 9, name: "Problem-solving", description: "Experienced in applying logical reasoning to tackle intricate challenges, a proficiency developed through extensive involvement in programming and systems analysis tasks.", image: "src/images/Problem Solving.png"},
-    {id: 10, name: "Teamwork", description: "Accustomed to collaborative environments, with a focus on projects that require team synergy and effective communication.", image: "src/images/Teamwork.png"},
-    {id: 11, name: "Attention to Detail", description: "Exhibit strong proficiency in time management and exceptional organizational abilities.", image: "src/images/Detail.png"},
-    {id: 12, name: "Eagerness to Learn", description: "Proactively pursuing new experiences and knowledge to enhance existing skills.", image: "src/images/Learning.png"},
+    {id: 1, name: "Programming Languages", description: "Experienced in a diverse range of programming languages, including but not limited to JavaScript, Kotlin, Java, C#, C, and C++. This broad knowledge base allows for flexibility and adaptability when tackling various software development tasks.", image: "src/images/programming.png"},
+    {id: 2, name: "Web Development", description: "Proficient in website design and development using HTML, CSS (SASS, Tailwind CSS, Bootstrap),JavaScript (React.js, Node.js), and C# (ASP.NET).", image: "src/images/webdev.png"},
+    {id: 3, name: "Data Management", description: "Familiar with data fundamentals, including SQL, ADO.NET, and Entity Framework.", image: "src/images/datamanagement.png"},
+    {id: 4, name: "Mobile Development", description: "Experienced in Java and Kotlin for Android development using Android Studio.", image: "src/images/mobiledev.png"},
+    {id: 5, name: "Version Control", description: "Proficient in Git, with experience in managing GitHub projects including branches and commits.", image: "src/images/versioncontrol.png"},
+    {id: 6, name: "Design Tools", description: "Proficient in Figma for interface design, prototyping, and real-time collaboration. Furthermore, I am adept at using Figma's vector editing capabilities and components feature to create reusable design elements, ensuring consistency across all designs.", image: "src/images/figma.png"},
+    {id: 7, name: "Professional Practices for IT", description: "Understanding of professional standards, ethics, and practices in the IT industry.", image: "src/images/it.png"},
+    {id: 8, name: "Project Management", description: "Experience collaborating projects from conception to completion, often a key component of coursework. ", image: "src/images/management.png"},
+    {id: 9, name: "Problem-solving", description: "Experienced in applying logical reasoning to tackle intricate challenges, a proficiency developed through extensive involvement in programming and systems analysis tasks.", image: "src/images/problemsolving.png"},
+    {id: 10, name: "Teamwork", description: "Accustomed to collaborative environments, with a focus on projects that require team synergy and effective communication.", image: "src/images/teamwork.png"},
+    {id: 11, name: "Attention to Detail", description: "Exhibit strong proficiency in time management and exceptional organizational abilities.", image: "src/images/detail.png"},
+    {id: 12, name: "Eagerness to Learn", description: "Proactively pursuing new experiences and knowledge to enhance existing skills.", image: "src/images/learning.png"},
 ]
 const Skills = () => {
     return (
