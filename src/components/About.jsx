f40a863843ead14a4ae520ce7bc8bb5d5d23c3a3
Guillermo Jimenez Iriarte <122047330+GuilleJI@users.noreply.github.com>
2025-03-13 15:28:35 -0300

    feat: update About component to enhance personal introduction and correct typos

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,11 +10,11 @@ const About = () => {
                 </div>
                 <div className='sm:w-1/2'>       
                     <p className='text-xl font-semibold mb-5'>About Me</p>
-                    <h2 className='md:text-5xl text-4xl font-bold'>To Start off...</h2>
+                    <h2 className='md:text-5xl text-4xl font-bold'>Allow me to introduce myself,</h2>
                     <p className=' text-lg mt-8 md:pr-8 mb-8'>
-                    Hi, I'm <strong className="text-primary">Guillermo</strong>—a dedicated programming graduating student with a passion for creating digital experiences that are both intuitive and impactful.  
+                    Hi, I'm <strong className="text-primary">Guillermo</strong>—a dedicated final-year programming student with a passion for creating digital experiences that are both intuitive and impactful.  
                     Born in <strong className="text-primary">Bolivia</strong>, I’ve embraced a rich, multicultural background, having spent my childhood in the <strong className="text-primary">United States</strong> before settling in <strong className="text-primary">Canada</strong> in 2012. 
-                    My technical expertise spans both <strong className="text-primary">front-end</strong> and <strong className="text-primary">back-end</strong> development, and I thrive in environemnts that push me to innovate and collaborate. 
+                    My technical expertise spans both <strong className="text-primary">front-end</strong> and <strong className="text-primary">back-end</strong> development, and I thrive in environments that push me to innovate and collaborate. 
                     <br/><br/>
                     I take pride in a <strong className="text-primary">user-first</strong> approach to <strong className="text-primary">design and development</strong>, seamlessly blending technical proficiency and creativity. Whether through team projects, hackathons or a hands-on professional work term project, I've honed my ability to tackle <strong className="text-primary">real-world challenges </strong> with agility and insight.
                     <br /><br/>
