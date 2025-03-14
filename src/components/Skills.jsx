d6b5f1b21035fefe6635ea95fcf42b7a44ae53c1
Guillermo Jimenez Iriarte <122047330+GuilleJI@users.noreply.github.com>
2025-03-14 16:04:31 -0300

    feat: update image paths to use the dist directory for consistency

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -2,29 +2,29 @@ import {Link} from 'react-scroll';
 
 // Making some array of objects for card items
 const techSkills =[
-    {id: 1, name: "Programming Languages", description: "Experienced in a diverse range of programming languages, including but not limited to JavaScript, C#, Kotlin, SQL, Java, and Python.", image: "public/images/Programming Languages.png"},
-    {id: 2, name: "Frameworks and Libraries", description: "Proficient in website design and development using frameworks like ASP.NET, WPF, React, Flask, Next.js, Node.js, and React Native.", image: "public/images/Frameworks.png"},
-    {id: 3, name: "Version Control", description: "Proficient in Git, with experience in managing GitHub projects including branches and commits.", image: "images/versioncontrol.png"}
+    {id: 1, name: "Programming Languages", description: "Experienced in a diverse range of programming languages, including but not limited to JavaScript, C#, Kotlin, SQL, Java, and Python.", image: "dist/images/Programming Languages.png"},
+    {id: 2, name: "Frameworks and Libraries", description: "Proficient in website design and development using frameworks like ASP.NET, WPF, React, Flask, Next.js, Node.js, and React Native.", image: "dist/images/Frameworks.png"},
+    {id: 3, name: "Version Control", description: "Proficient in Git, with experience in managing GitHub projects including branches and commits.", image: "dist/images/versioncontrol.png"}
     ,
-    {id: 4, name: "Design Tools", description: "Proficient in Figma for interface design, prototyping, and real-time collaboration. Adept at leveraging its vector editing tools and component features to create reusable design elements that ensure visual consistency across projects.", image: "images/figma.png"}
+    {id: 4, name: "Design Tools", description: "Proficient in Figma for interface design, prototyping, and real-time collaboration. Adept at leveraging its vector editing tools and component features to create reusable design elements that ensure visual consistency across projects.", image: "dist/images/figma.png"}
     ,
-    {id: 5, name: "AI and Machine Learning", description: "Experienced in developing projects utilizing NLP models and pipelines, leveraging community libraries such as Hugging Face.", image:"public/images/HuggingFace.png"},
-    {id: 6, name: "Databases", description:"Skilled in managing and optimizing databases, with experience in SQLite, PostgreSQL, MySQL, and Microsoft SQL Server.", image: "public/images/Database.png"}
+    {id: 5, name: "AI and Machine Learning", description: "Experienced in developing projects utilizing NLP models and pipelines, leveraging community libraries such as Hugging Face.", image:"dist/images/HuggingFace.png"},
+    {id: 6, name: "Databases", description:"Skilled in managing and optimizing databases, with experience in SQLite, PostgreSQL, MySQL, and Microsoft SQL Server.", image: "dist/images/Database.png"}
     
 ]
 
 const softSkills = [
-    {id: 1, name: "Teamwork-Oriented", description: "With a background in hackathons and collaborative projects, I've seen firsthand how teamwork drives success and achieves meaningful goals. I value diverse perspectives from peers and focus on delivering high-quality results through cooperation and support. My aim is to contribute to a positive impact by helping the team thrive.", image:"images/Team.png"}
+    {id: 1, name: "Teamwork-Oriented", description: "With a background in hackathons and collaborative projects, I've seen firsthand how teamwork drives success and achieves meaningful goals. I value diverse perspectives from peers and focus on delivering high-quality results through cooperation and support. My aim is to contribute to a positive impact by helping the team thrive.", image:"dist/images/Team.png"}
     ,
-    {id: 2, name: "Active Listening", description: "Staying alert and proactive in discussions is a strength I bring to any team. I make it a priority to fully understand project and team needs before acting, which allows me to contribute effectively to collaboration and decision-making. By staying engaged, I help ensure our collective efforts are well-informed and aligned.", image:"images/Active Listening.png"}
+    {id: 2, name: "Active Listening", description: "Staying alert and proactive in discussions is a strength I bring to any team. I make it a priority to fully understand project and team needs before acting, which allows me to contribute effectively to collaboration and decision-making. By staying engaged, I help ensure our collective efforts are well-informed and aligned.", image:"dist/images/Active Listening.png"}
     ,
-    {id: 3, name: "Willingness to Learn", description: "I'm passionate about learning new tools and technologies to keep my skills sharp and relevant. Curiosity drives me to grow, not just for myself, but to bring fresh ideas and innovation to projects. This commitment supports my ability make a positive impact alongside my teammates.", image: "images/Learn.png"}
+    {id: 3, name: "Willingness to Learn", description: "I'm passionate about learning new tools and technologies to keep my skills sharp and relevant. Curiosity drives me to grow, not just for myself, but to bring fresh ideas and innovation to projects. This commitment supports my ability make a positive impact alongside my teammates.", image: "dist/images/Learn.png"}
     ,
-    {id: 4, name: "Effective Communication (English & Spanish)", description: "Clear communication with both technical an non-technical audiences is something I've honed through past projects and brainstorming sessions. Being bilingual in English and Spanish enhances my ability to connect teams and stakeholders, ensuring smooth interactions and shared understanding. It's all about building bridges for better collaboration.", image:"images/Communication.png"}
+    {id: 4, name: "Effective Communication (English & Spanish)", description: "Clear communication with both technical an non-technical audiences is something I've honed through past projects and brainstorming sessions. Being bilingual in English and Spanish enhances my ability to connect teams and stakeholders, ensuring smooth interactions and shared understanding. It's all about building bridges for better collaboration.", image:"dist/images/Communication.png"}
     ,
-    {id: 5, name: "Creativity", description: "I love bringing creative ideas to mobile and web development, as well as UI design, with focus on improving functionality and user experience. Innovative thinking helps me turn challenges into practical solutions, and I enjoy sharing perspectives that can spark inspiration within the team.", image: "public/images/Creative.png"}
+    {id: 5, name: "Creativity", description: "I love bringing creative ideas to mobile and web development, as well as UI design, with focus on improving functionality and user experience. Innovative thinking helps me turn challenges into practical solutions, and I enjoy sharing perspectives that can spark inspiration within the team.", image: "dist/images/Creative.png"}
     ,
-    {id: 6, name: "Initiative", description: "I take a proactive approach to sprint requirements and seeking out ways to add value, drawing from my academic and project experiences. Consistency and impact matter to me, and I strive to support the team's goals by stepping up when opportunities arise.", image: "public/images/Initiative.png"}
+    {id: 6, name: "Initiative", description: "I take a proactive approach to sprint requirements and seeking out ways to add value, drawing from my academic and project experiences. Consistency and impact matter to me, and I strive to support the team's goals by stepping up when opportunities arise.", image: "dist/images/Initiative.png"}
 
 ]
 
